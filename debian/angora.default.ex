9c0f7cd8fcc57f16043a4687b712a927c3faf09f
Ruben Undheim <ruben.undheim@gmail.com>
2015-01-29 00:13:47 +0100

    Added debian folder

---
+++ debian/angora.default.ex
@@ -0,0 +1,10 @@
+# Defaults for angora initscript
+# sourced by /etc/init.d/angora
+# installed at /etc/default/angora by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(angora):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="angora" command="/usr/bin/angora"
