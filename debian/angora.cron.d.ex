1ba4859a619d940faa9bb82bac924133b79d0660
Ruben Undheim <ruben.undheim@gmail.com>
2015-02-04 23:30:04 +0100

    Fixing lintians

--- debian/angora.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the angora package
-#
-0 4	* * *	root	[ -x /usr/bin/angora_maintenance ] && /usr/bin/angora_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(angora):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="angora" command="/usr/bin/angora"
